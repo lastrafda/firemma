fad7b646961bf6c43b1939045f4aac25692c5b70
José Lastra <lastrafda@gmail.com>
2020-10-11 21:03:53 -0300

    fix scss build

--- webpack.config.js
+++ webpack.config.js
@@ -2,59 +2,66 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: './src/entry.js',
-  output: {
-    path: path.resolve(__dirname, 'output'),
-    filename: 'my-precious.webpack.bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(jsx|js)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-env',
-                {
-                  targets: '> 0.25%, not dead',
-                },
+module.exports = function (_env, argv) {
+  const isProduction = _env.NODE_ENV === 'production';
+  const isDevelopment = !isProduction;
+
+  console.log('isDev', isDevelopment);
+  return {
+    entry: './src/entry.js',
+    output: {
+      path: path.resolve(__dirname, 'output'),
+      filename: 'my-precious.webpack.bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(jsx|js)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    targets: '> 0.25%, not dead',
+                  },
+                ],
+                '@babel/preset-react',
               ],
-              '@babel/preset-react',
-            ],
+            },
           },
         },
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          process.env.NODE_ENV !== 'production'
-            ? 'style-loader'
-            : MiniCssExtractPlugin,
-          'css-loader',
-          'sass-loader',
-        ],
-      },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            isDevelopment
+              ? 'style-loader'
+              : MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+    devServer: {
+      contentBase: path.resolve(__dirname, 'public'),
+      clientLogLevel: 'silent',
+      compress: true,
+      open: true,
+      port: 7777,
+      hot: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public/index.html'),
+        inject: true,
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'wtf-[name].css',
+        chunkFilename: 'wtf-[id].css',
+      }),
     ],
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'public'),
-    clientLogLevel: 'silent',
-    compress: true,
-    open: true,
-    port: 7777,
-    hot: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public/index.html'),
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'wtf-[name].css',
-      chunkFilename: 'wtf-[id].css',
-    }),
-  ],
+  };
 };
